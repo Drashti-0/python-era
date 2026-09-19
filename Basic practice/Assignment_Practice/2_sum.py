da8b1e2e29b6dfa34e1cc2758672accbfe8b75e8
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-09-19 09:02:59 +0530

    basic start

---
+++ Basic practice/Assignment_Practice/2_sum.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+c=a+b
+
+print("The sum is:", c)
+print("Substraction is:", a-b)
+print("Multiplication is:", a*b)
+print("Division is:", a/b)
+print("quotient is:", a//b)
