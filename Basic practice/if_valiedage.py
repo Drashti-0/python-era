f3b30fdd1e8f6ad3595a4b9457c39ce7119a8622
Drashti-0 <25cs003@charusat.edu.in>
2026-09-19 11:13:03 +0530

    if statement

---
+++ Basic practice/if_valiedage.py
@@ -0,0 +1,23 @@
+''' age=int(input("Enter your age"))
+
+if age>=18:
+    print("You can vote")
+    
+else:
+    print("you can not to vote") '''
+    
+    
+    
+age =int(input("Enter yor age: "))
+
+if age<13:
+    print("Chiled")
+    
+    
+    
+elif age>=13 and age<=18 :
+    print("Tenager")
+    
+    
+else:
+    print("Adult")
