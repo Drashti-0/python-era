92ca104dd142324ef4eb718d5948d631da0535a5
Drashti-0 <25cs003@charusat.edu.in>
2026-09-19 11:07:40 +0530

    if practice

---
+++ Basic practice/if.py
@@ -0,0 +1,14 @@
+color = input("Enter your color: ")
+
+if color =="red":
+    print("stop")
+    
+elif color=="green":
+    print("go")
+    
+elif color=="yellow":
+    print("Look")
+    
+    
+else:
+    print("Wrong color for traffic light")
