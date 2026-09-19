4af1a4930bdf5df404bacaa15598edc036289712
Drashti-0 <25cs003@charusat.edu.in>
2026-09-19 11:33:17 +0530

    some changes and add code

--- Basic practice/if_checkdetails.py
+++ Basic practice/if_checkdetails.py
@@ -1,4 +1,4 @@
-username = input("Enter your valied username: ")
+''' username = input("Enter your valied username: ")
 password = input("Enter your valied password: ")
 
 if username=="Admin" and password=="Admin123":
@@ -11,4 +11,22 @@ elif password!="Admin123":
     print("Invalied Password")
     
 else:
-    print("Your detils is incorrected")
+    print("Your detils is incorrected") '''
+    
+    
+    #using nested if else
+    
+    
+username = input("Enter your valied username: ")
+password = input("Enter your valied password: ")
+
+
+if username=="Admin" and password=="Admin123":
+    print("You are successfully login in")
+
+else:
+    if username != "Admin":
+        print("Your username is incorrect")
+    
+    else:
+        print("Password is incorrect")
