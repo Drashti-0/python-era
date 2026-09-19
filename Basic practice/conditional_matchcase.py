2a103dafa43e496882d545f9c02f5e1ab1c08723
Drashti-0 <25cs003@charusat.edu.in>
2026-09-19 13:09:59 +0530

    matchcse exploring

---
+++ Basic practice/conditional_matchcase.py
@@ -0,0 +1,13 @@
+color = input("Eter your color")
+
+match color:
+    
+    case "green":
+        print("go")
+        
+    case "yellow":
+        print("look")
+        
+    case "red":
+        print("stop")         
+        
