1e9d6e8024a8311e3297d3674d0d468efdb9b893
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-09-19 09:01:01 +0530

    basic practice with grateful starting

---
+++ Basic practice/operators.py
@@ -0,0 +1,43 @@
+
+#arithmetic
+
+a=6
+b=4
+
+print("addition:", a+b)
+print("subtraction:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print("               ")
+
+
+#relational
+
+print("greater than:", a>b)
+print("less than:", a<b)
+print("equal to:", a==b)
+print("not equal to:", a!=b)
+print("greater than or equal to:", a>=b)
+print("less than or equal to:", a<=b)
+print("               ")
+
+#assignment
+
+c=10
+
+print("assignment:", c)
+
+c+=5
+
+print("addition assignment:", c)
+
+print("               ")
+
+#logical
+
+x=True
+y=False
+
+print("logical not:", not x)
+print("logical and:", x and y)
+print("logical or:", x or y)        
