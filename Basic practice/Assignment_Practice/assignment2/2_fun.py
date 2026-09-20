85d15283e45e58300c66e88dcdbe3605158d3049
Drashti-0 <25cs003@charusat.edu.in>
2026-09-20 22:22:29 +0530

    changes

---
+++ Basic practice/Assignment_Practice/assignment2/2_fun.py
@@ -0,0 +1,12 @@
+a=int(input("Enter number a: "))
+b=int(input("Enter number b: "))
+
+def function(a,b):
+    
+    for c in range(a,b):
+        if c%2==0:
+            print("Even number is : ",c)
+            
+function(a,b)           
+            
+        
---
+++ Basic practice/Assignment_Practice/assignment2/3_count.py
@@ -0,0 +1,17 @@
+n=int(input("Enter number n"))
+
+
+def function(n):
+    
+  
+while(n!=0):    
+    reminder=n%10
+    print(n)
+      n=n//10
+      
+      
+      
+       
+    
+
+function(n)
