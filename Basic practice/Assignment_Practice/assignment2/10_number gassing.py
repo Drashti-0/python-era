f502cf068d938ea447a6eebbff8ce5028deb1cf6
Drashti-0 <25cs003@charusat.edu.in>
2026-09-22 08:02:15 +0530

    chnagening

--- Basic practice/Assignment_Practice/assignment2/10_number gassing.py
+++ Basic practice/Assignment_Practice/assignment2/10_number gassing.py
@@ -1,1 +1,15 @@
-n=int
+
+while True:
+    
+    n=int(input("enter number: "))
+    a=57
+    
+    if n==a:
+        print("You gusse correct word!")
+        break
+    
+    if(n>a):
+        print("Too High")
+    elif(n<a):
+        print("Too Low")
+
--- Basic practice/Assignment_Practice/assignment2/9_qn.py
+++ Basic practice/Assignment_Practice/assignment2/9_qn.py
@@ -7,5 +7,5 @@ a=int(input("Enter ending Range"))
 
 def funcation(n,a):
     for n in range(2,a):
-        if n>1:
+        if n>1: 
             
