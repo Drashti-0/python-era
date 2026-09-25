ce5f176686a581117d6e82a9d8d1df355cad380e
Drashti-0 <25cs003@charusat.edu.in>
2026-09-25 05:40:28 +0530

    practice

---
+++ SELF_practice/basic_dictonary.py
@@ -0,0 +1,24 @@
+student={
+    "name":"Drashti",
+    "age":19,
+    "branch":"CSE",
+    "semester":3,
+    "cgpa":7.98
+   
+}
+
+#key value
+dict_keys=student.keys()
+print(dict_keys)
+print(" ")
+
+
+#print value
+dict_value=list(student.values())
+print(dict_value)
+print(type(dict_value))
+print(" ")
+
+
+#both print
+print(student.items())
