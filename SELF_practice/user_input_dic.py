9ed7d6769c9fc213c5abef25411b0487776d1b0a
Drashti-0 <25cs003@charusat.edu.in>
2026-09-25 20:37:30 +0530

    code practice

--- SELF_practice/user_input_dic.py
+++ SELF_practice/user_input_dic.py
@@ -1,2 +1,11 @@
 student={}
 
+for i in range(3):
+    name=input("Enter your name: ")
+    marks=float(input("Enter your marks: "))
+    
+    #no print repeted value
+    #like value dhruti dhruti 
+    student[name]=marks
+print(student)
+
