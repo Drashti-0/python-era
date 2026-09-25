c9f7a8b529c491b1892bc8cdeecf07166c402e97
Drashti-0 <25cs003@charusat.edu.in>
2026-09-25 05:40:56 +0530

    code practice

---
+++ SELF_practice/conerts_listtotuple_bothside.py
@@ -0,0 +1,14 @@
+#convert list to tuple
+tuple1 = (10, 20, 30, 40, 50)
+
+list1 = list(tuple1)
+print(list1)
+print(type(list1))
+
+
+#converts list to tuple
+
+list2 = [10, 20, 30, 40, 50]
+tuple2=tuple(list2)
+print(tuple2)
+print(type(tuple2))
---
+++ SELF_practice/dic_add.py
@@ -0,0 +1,14 @@
+student={
+    "name":"Drashti",
+    "age":19,
+    "branch":"CSE",
+    "semester":3,
+    "cgpa":7.98
+   
+}
+
+student.update({"clg" : "Charusat"})
+print(student)
+
+student.pop("age")
+print(student)
