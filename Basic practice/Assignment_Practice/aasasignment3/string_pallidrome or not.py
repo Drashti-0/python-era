f3d2477ac8ad9d8f3667bf21a71df0e043f366e4
Drashti-0 <25cs003@charusat.edu.in>
2026-09-26 14:22:20 +0530

    practice with python

--- Basic practice/Assignment_Practice/aasasignment3/string_pallidrome or not.py
+++ Basic practice/Assignment_Practice/aasasignment3/string_pallidrome or not.py
@@ -1,1 +1,12 @@
-a=input
+a=input("Enter your string: ")
+
+print(a)
+#reverse the string
+print(a[::-1])
+
+if(a==a[::-1]):
+    print("String is pallidrome")
+    
+else:
+    print("String is not pallidrome")
+    
