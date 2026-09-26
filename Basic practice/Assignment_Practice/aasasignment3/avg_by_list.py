cd321e7186fb57f4b6912a2837782d859018ea6e
Drashti-0 <25cs003@charusat.edu.in>
2026-09-26 14:23:28 +0530

    practice

---
+++ Basic practice/Assignment_Practice/aasasignment3/avg_by_list.py
@@ -0,0 +1,8 @@
+list=[5, 10, 15, 20, 25]
+
+sum=0
+
+for i in list:
+    sum+=i
+    
+print((sum)/len(list))
---
+++ Basic practice/Assignment_Practice/aasasignment3/mergebylist.py
@@ -0,0 +1,8 @@
+list1 = [1, 2, 7]
+list2 = [2, 4, 5]
+
+list3=list1+list2
+print(list3)
+
+list3.sort()
+print(list3)
